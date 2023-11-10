ea3d4ee34c76516e5d5ff8939519df1fb4beeecb
Alexandre Gaia <alexandregaia911@gmail.com>
2023-11-10 20:41:48 -0300

    creating sections - devices/faq/header/footet/mobile responsiveness

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const heroHeight = heroSection.clientHeight;
+
+    window.addEventListener('scroll', ()=>{
+        currentPos = window.scrollY
+
+        if(currentPos < heroHeight){
+            hiddingHeader()
+        } else {
+            showHeaderEl()
+        }
+    })
+
+    //shows, tabs functionalities
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', (btn)=>{
             const targetTab = btn.target.dataset.tabButton;
@@ -11,7 +26,27 @@ document.addEventListener('DOMContentLoaded', ()=>{
             btn.target.classList.add('shows__tabs__button--is-active');
         })
     }
+    //faq section, accordion
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', showOrHideQuestion);[]
+    }
 })
+
+function hiddingHeader(){
+    const header = document.querySelector('.header')
+    header.classList.add('header--is-hidden')
+}
+function showHeaderEl(){
+    const header = document.querySelector('.header')
+    header.classList.remove('header--is-hidden')
+}
+
+function showOrHideQuestion(element){
+    const classe = 'faq__questions__item--is-open';
+    const daddyElement = element.target.parentNode;
+
+    daddyElement.classList.toggle(classe)
+}
 
 function removeActiveBtn(){
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -28,3 +63,5 @@ function hideAllTabs(){
         sectionTab[i].classList.remove('shows__list--is-active');
     }
 }
+
+
